ca2c67cb983b8d36adc01e63087cf3280e9b7bac
nekena86 <hei.nekena.6@gmail.com>
2026-04-21 14:12:36 +0300

    initial commit

---
+++ src/main/java/hei/school/tdfinalprog3/TdFinalProg3Application.java
@@ -0,0 +1,13 @@
+package hei.school.tdfinalprog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdFinalProg3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TdFinalProg3Application.class, args);
+    }
+
+}
